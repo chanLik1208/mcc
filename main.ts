482b7dbf93b203a6b8821afd68a68b93b722b3d2
chanLik1208 <92614601+chanLik1208@users.noreply.github.com>
2022-01-04 13:49:04 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,88 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        # . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    music.playMelody("C - - - - - - - ", 120)
+    basic.showLeds(`
+        # . . . .
+        . . . . .
+        # . . . .
+        . . . . .
+        . . . . .
+        `)
+    music.playMelody("C - - - - - - - ", 120)
+    basic.showLeds(`
+        # . . . .
+        . . . . .
+        # . . . .
+        . . . . .
+        # . . . .
+        `)
+    music.playMelody("C - - - - - - - ", 120)
+    music.playMelody("C5 - - - - - - - ", 120)
+    control.waitMicros(10)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
+input.onPinPressed(TouchPin.P1, function () {
+    music.playMelody("C5 B A G F E D C ", 120)
+    basic.showLeds(`
+        # . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # . # . .
+        . . . . #
+        . # # . .
+        # . . . .
+        . . . . #
+        `)
+    basic.showLeds(`
+        . . # . #
+        . # . . .
+        # . . . .
+        . . . . #
+        . . # . .
+        `)
+    for (let index = 0; index < 4; index++) {
+    	
+    }
+    control.waitMicros(10)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
+pins.touchSetMode(TouchTarget.P0, TouchTargetMode.Capacitive)
+basic.forever(function () {
+	
+})
